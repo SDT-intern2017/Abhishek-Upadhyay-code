7eb9fd92a251f46f3cb4706a125120edebf2dc9c
Mallika Chaudhary <sdtintern2017@gmail.com>
2017-07-18 22:26:00 -0700

    Update tweet.py
    Removed WhiteSpaces.

--- tweet.py
+++ tweet.py
@@ -1,10 +1,6 @@
 import tweepy 
 from key import *
-
-
 auth=tweepy.OAuthHandler(ckey,csecret)
 auth.set_access_token(akey, asecret)
-
 api=tweepy.API(auth)
-
 api.update_with_media('in.pinterest.com.jpeg','Tweeting Lord Shiv with python ')
